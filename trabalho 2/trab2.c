42ff5ff1dfbaf7e246c7f01a61ff4f52fa139a78
gabrieldal <88405111+gabrieldal@users.noreply.github.com>
2022-01-26 16:00:07 -0300

    união
    união de duas funções em uma só

--- trabalho 2/trab2.c
+++ trabalho 2/trab2.c
@@ -12,34 +12,35 @@ struct aluno
 
 struct aluno *start(struct aluno *head)
 {
-
-    head = malloc(sizeof(struct aluno));
-    printf("Digite o nome do aluno: \n");
-    scanf("%s", &head->nome);
-    printf("Digite a matricula do aluno: \n");
-    scanf("%i", &head->matricula);
-    printf("Digite a nota do aluno: \n");
-    scanf("%f", &head->nota);
-    head->link = NULL;
-    return head;
-}
-
-void add_node(struct aluno *head)
-{
-    struct aluno *pont1, *pont2;
-    pont1 = head;
-    pont2 = malloc(sizeof(struct aluno));
-    printf("Digite o nome do aluno: \n");
-    scanf("%s", &pont2->nome);
-    printf("Digite a matricula do aluno: \n");
-    scanf("%i", &pont2->matricula);
-    printf("Digite a nota do aluno: \n");
-    scanf("%f", &pont2->nota);
-    while (pont1->link != NULL)
+    if (head == NULL)
     {
-        pont1 = pont1->link;
+        head = malloc(sizeof(struct aluno));
+        printf("Digite o nome do aluno: \n");
+        scanf("%s", &head->nome);
+        printf("Digite a matricula do aluno: \n");
+        scanf("%i", &head->matricula);
+        printf("Digite a nota do aluno: \n");
+        scanf("%f", &head->nota);
+        head->link = NULL;
     }
-    pont1->link = pont2;
+    else
+    {
+        struct aluno *pont1, *pont2;
+        pont1 = head;
+        pont2 = malloc(sizeof(struct aluno));
+        printf("Digite o nome do aluno: \n");
+        scanf("%s", &pont2->nome);
+        printf("Digite a matricula do aluno: \n");
+        scanf("%i", &pont2->matricula);
+        printf("Digite a nota do aluno: \n");
+        scanf("%f", &pont2->nota);
+        while (pont1->link != NULL)
+        {
+            pont1 = pont1->link;
+        }
+        pont1->link = pont2;
+    }
+    return head;
 }
 
 void print_node(struct aluno *head)
@@ -57,5 +58,20 @@ void print_node(struct aluno *head)
         printf("Nota: %.2f\n", pont1->nota);
         printf("*******************\n");
         pont1 = pont1->link;
+    }
+}
+void sortlist()
+{
+    char n[40];
+    struct aluno *pont1 = head;
+    struct aluno *pont2 = pont1->next;
+    while (pont1 != NULL)
+    {
+        if (strcmp(pont1->nome, pont2->nome) > 0)
+        {
+            strcpy(n, pont2->nome);
+            strcpy(pont2->nome, pont1->nome);
+            strcpy(pont1->nome, n);
+        }
     }
 }
--- trabalho 2/trab2.h
+++ trabalho 2/trab2.h
@@ -1,4 +1,3 @@
 struct aluno;
 struct aluno *start(struct aluno *head);
-void add_node(struct aluno *head);
 void print_node(struct aluno *head);
